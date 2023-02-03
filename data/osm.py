6a0ae2075150e01f574cf86ae6c9b1395c5395ab
macbre <maciej.brencz@gmail.com>
2023-02-03 19:04:40 +0000

    Search for ferries and helipads

--- data/osm.py
+++ data/osm.py
@@ -18,8 +18,19 @@ DIR = path.abspath(path.dirname(__file__))
 
 # https://wiki.openstreetmap.org/wiki/Buses#Stops_and_bus_stations
 # <tag k="highway" v="bus_stop"/>
-TAG_KEY = 'highway'
-TAG_VALUE = 'bus_stop'
+# TAG_KEY = 'highway'
+# TAG_VALUE = 'bus_stop'
+
+# https://wiki.openstreetmap.org/wiki/Tag:aeroway%3Dhelipad
+# <tag k="aeroway" v="helipad"/>
+# TODO: handle <way> tags
+# TAG_KEY = 'aeroway'
+# TAG_VALUE = 'helipad'
+
+# https://wiki.openstreetmap.org/wiki/Tag:amenity%3Dferry_terminal
+# <tag k="amenity" v="ferry_terminal"/>
+TAG_KEY = 'amenity'
+TAG_VALUE = 'ferry_terminal'
 
 def cache_osm_file():
     # https://download.geofabrik.de/europe/faroe-islands.html
@@ -133,7 +144,7 @@ def main():
         # <tag k="highway" v="bus_stop"/>
         matches = [
             (TAG_KEY, TAG_VALUE),
-            ('bus', 'yes'),
+            # ('bus', 'yes'),
         ]
 
         for (key, value) in node_tags:
@@ -150,7 +161,7 @@ def main():
     iterate_xml(local_file, node_callback)
 
     # write a GeoJSON file
-    geojson_file = path.join(DIR, f'{TAG_VALUE}-{TAG_VALUE}.json')
+    geojson_file = path.join(DIR, f'{TAG_KEY}-{TAG_VALUE}.json')
     logger.info(f'Writing {len(nodes)} node(s) GeoJSON to {geojson_file} ...')
 
     with open(geojson_file, 'wt') as f:
