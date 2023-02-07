0a0f4cf378f43269844a09b182f3b0c81e06d286
macbre <maciej.brencz@gmail.com>
2023-02-07 13:05:05 +0000

    Add churches map

--- data/arcgis.py
+++ data/arcgis.py
@@ -9,24 +9,37 @@ from typing import Generator
 import requests
 
 # https://gis.us.fo/arcgis/rest/services/lendiskort/us_lendiskort/MapServer/48
-FOLDER = 'lendiskort'
-SERVICE = 'us_lendiskort'
-MAP_ID = 48
-LAYER = 'Tyrluplass'  # can be a substring match too
+# FOLDER = 'lendiskort'
+# SERVICE = 'us_lendiskort'
+# MAP_ID = 48
+# PROPETY_NAME = 'layer'
+# PROPERTY_VALUE = 'Tyrluplass'  # can be a substring match too
+
+
+# https://gis.us.fo/arcgis/rest/services/poi/us_poi/MapServer/layers
+FOLDER = 'poi'
+SERVICE = 'us_poi'
+MAP_ID = 19
+PROPERTY_NAME = 'poi_icon'
+PROPERTY_VALUE = 'church'  # "poi_icon": "church",
 
 ACGIS_URL = f'https://gis.us.fo/arcgis/rest/services/{FOLDER}/{SERVICE}/MapServer/{MAP_ID}/query';
 
 DIR = path.abspath(path.dirname(__file__))
-GEOJSON_FILE = path.join(DIR, '..', 'geojson', f'us-{FOLDER}-{SERVICE}-{MAP_ID}-{LAYER}.json')
+GEOJSON_FILE = path.join(DIR, '..', 'geojson', f'us-{FOLDER}-{SERVICE}-{MAP_ID}-{PROPERTY_VALUE or "main"}.json')
 
 
 def filter_feature(feature: dict) -> bool:
     """
     Make get_features_from_arcgis() yield only the features we're interested in
     """
+    # no PROPETY_NAME provided, no filtering then
+    if PROPERTY_NAME is None:
+        return True
+
     props = feature.get('properties', {})
-    layer = props.get('layer')
-    return layer and LAYER in layer
+    prop_value = props.get(PROPERTY_NAME)
+    return prop_value and PROPERTY_VALUE in prop_value
 
 
 def from_polygon_to_point(feature: dict) -> dict:
@@ -57,8 +70,11 @@ def from_polygon_to_point(feature: dict) -> dict:
         ]
     }
     """
-    polygon_points = feature.get('geometry', {}).get('coordinates')
+    geometry_type = feature.get('geometry', {}).get('type')
+    if geometry_type != 'Polygon':
+        return feature
 
+    polygon_points = feature.get('geometry', {}).get('coordinates')
     if not polygon_points:
         return feature
 
@@ -82,6 +98,7 @@ def get_features_from_arcgis(url: str) -> Generator:
     while True:
         logger.info(f'Offset at #{offset}')
 
+        # e.g. https://gis.us.fo/arcgis/rest/services/adressur/us_adr_husanr/FeatureServer/0/query
         query_params = {
             'f': 'geojson',
             'where': '1=1',
@@ -113,7 +130,8 @@ def get_features_from_arcgis(url: str) -> Generator:
 
 def main():
     logger = logging.getLogger('arcgis')
-    logger.info(f'Using {FOLDER}/{SERVICE} with map #{MAP_ID} and "{LAYER}" layer filtering ...')
+    logger.info(f'Using {FOLDER}/{SERVICE} with map #{MAP_ID} and "{PROPERTY_NAME}" = "{PROPERTY_VALUE}" filtering ...')
+    logger.info(f'Features will be stored in {GEOJSON_FILE}')
 
     features = list(get_features_from_arcgis(ACGIS_URL))
 
