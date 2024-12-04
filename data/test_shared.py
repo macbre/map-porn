e242c071b1e2972d9e023318bc35fd3e281bbb9a
macbre <maciej.brencz@gmail.com>
2024-12-04 22:16:28 +0000

    Add the basic code coverage for the Node dataclass

---
+++ data/test_shared.py
@@ -0,0 +1,35 @@
+import json
+from shared import Node
+
+
+TEST_LAT = 61.5257422
+TEST_LON = -6.879629
+
+
+def get_test_node() -> Node:
+    return Node(
+        lat=TEST_LAT,
+        lon=TEST_LON,
+        tags=(
+            ('foo', 'bar'),
+        )
+    )
+
+
+def test_node():
+    node = get_test_node()
+
+    assert node.lat == TEST_LAT
+    assert node.lon == TEST_LON
+
+
+def test_node_to_json():
+    node = get_test_node()
+    geojson = node.to_geojson()
+
+    assert geojson['type'] == 'Feature'
+
+    assert geojson['geometry']['type'] == 'Point'
+    assert geojson['geometry']['coordinates'] == [TEST_LON, TEST_LAT]
+
+    assert geojson['properties']['foo'] == 'bar'
